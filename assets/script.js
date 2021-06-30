2f04d7e63ac7747a4a6faa6ffa6b858f93e1a822
Vivi Cowan <vivicare@gmail.com>
2021-06-29 19:44:38 -0700

    created a for loop to add information to 5 cards

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,8 @@
 var apiKey = "78a975a991c3d2cafd3350be28944d97";
 
-function forecastWeather(city) {
-	var currentUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + apiKey;
+// function forecastWeather(city) {
+	// var currentUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + apiKey;
+	var currentUrl = "https://api.openweathermap.org/data/2.5/weather?q=London&units=imperial&appid=" + apiKey;
 
 	fetch(currentUrl)
 	.then(function (response) {
@@ -24,9 +25,36 @@ function forecastWeather(city) {
 			.then(function (forecast) {
 	   	console.log(forecast);
 
+			for (var i = 0; i < 5; i++) {
+				var tempF = (forecast.daily[i].temp.day - 273.15) * 1.8 + 32;
+
+				$("#card-body" + i).find("#temp").text(tempF.toFixed(2) + "°F");
+				$("#card-body" + i).find("#wind").text(forecast.daily[i].wind_speed + " mph");
+				$("#card-body" + i).find("#humidity").text(forecast.daily[i].humidity + " %");
+
+				var iconUrl =
+				"http://openweathermap.org/img/wn/" + forecast.daily[i].weather[0].icon + "@2x.png";
+	 
+				var iconImg = $("<img>").attr({
+					src: iconUrl,
+					alt: forecast.daily[i].weather[0].icon,
+				});
+			
+				$("#card-body" + i).find("#weather-icon").empty().append(iconImg);
+
+				var unixFormat = moment.unix(forecast.daily[i].dt).format("M/DD/YYYY");
+
+				$("#card-body" + i).find("#city-date").text(unixFormat);
+
+			 }
+
+			
+			
 
 
 
 	   })
   })
-}
+// }
+
+
